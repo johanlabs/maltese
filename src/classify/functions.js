4bf7a9acd3867dff8f9a5018fb3c3ea6621add61
João Sant'Ana <viitorsant@gmail.com>
2025-12-17 15:07:04 -0300

    v2 - include integration

--- src/classify/functions.js
+++ src/classify/functions.js
@@ -1,176 +1,134 @@
 const validator = require('validator');
 const nlp = require('compromise');
-
 const _ = require('lodash');
 const citiesData = require('cities.json');
 
 const knownCities = _.map(_.filter(citiesData), 'name');
 
-function identifyNames(text) {
-    const nameRegex = /\b[A-ZÀ-ÿ][a-zà-ÿ]+(?:\s[A-ZÀ-ÿ][a-zà-ÿ]+)+\b/g;
-    const names = [];
-    const doc = nlp(text);
-    const nlpNames = doc.people().out('array');
+/**
+ * Helpers
+ */
+function uniquePush(arr, value) {
+    if (!arr.includes(value)) {
+        arr.push(value);
+    }
+}
 
-    const nameList = [...(text.match(nameRegex) || []), ...nlpNames]
-        .filter(name => /^[A-Za-zÀ-ÿ\s]+$/.test(name) && name.length > 1)
-        .flatMap(name => name.toLowerCase().includes(' e ') ? name.split(' e ').map(n => n.trim()) : [name]);
+function matchUnique(text, regex, validateFn) {
+    const results = [];
+    const matches = text.match(regex) || [];
 
-    nameList.forEach(name => {
-        if (!names.includes(name)) {
-            names.push(name);
+    matches.forEach(value => {
+        if (!validateFn || validateFn(value)) {
+            uniquePush(results, value);
         }
     });
+
+    return results;
+}
+
+/**
+ * Identifiers
+ */
+function identifyNames(text) {
+    const nameRegex = /\b[A-ZÀ-ÿ][a-zà-ÿ]+(?:\s[A-ZÀ-ÿ][a-zà-ÿ]+)+\b/g;
+    const doc = nlp(text);
+    const nlpNames = doc.people().out('array');
+    const names = [];
+
+    const candidates = [
+        ...(text.match(nameRegex) || []),
+        ...nlpNames
+    ]
+        .filter(name =>
+            /^[A-Za-zÀ-ÿ\s]+$/.test(name) &&
+            name.length > 1
+        )
+        .flatMap(name =>
+            name.toLowerCase().includes(' e ')
+                ? name.split(' e ').map(n => n.trim())
+                : [name]
+        );
+
+    candidates.forEach(name => uniquePush(names, name));
 
     return names;
 }
 
 function identifyDates(text) {
     const dateRegex = /(\b\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}\b)|(\b\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}\b)|(\b\d{1,2}\s+de\s+(?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)\b)|(\b(?:Janeiro|Fevereiro|Março|Abril|Maio|Junho|Julho|Agosto|Setembro|Outubro|Novembro|Dezembro)\s+de\s\d{4}\b)/gi;
-    const dates = [];
-    (text.match(dateRegex) || []).forEach(date => {
-        if (!dates.includes(date)) {
-            dates.push(date);
-        }
-    });
-    return dates;
+    return matchUnique(text, dateRegex);
 }
 
 function identifyTimes(text) {
     const timeRegex = /\b(?:\d{1,2}:\d{2}(?::\d{2})?(?:\s?[ap]m)?)/gi;
-    const times = [];
-    (text.match(timeRegex) || []).forEach(time => {
-        if (!times.includes(time)) {
-            times.push(time);
-        }
-    });
-    return times;
+    return matchUnique(text, timeRegex);
 }
 
 function identifyAddresses(text) {
     const addressRegex = /\b(?:Rua|Avenida|AV\.|Alameda|Travessa|Praça)\s+[\w\s\d.-]+/gi;
-    const addresses = [];
-    (text.match(addressRegex) || []).forEach(addr => {
-        if (!addresses.includes(addr.trim())) {
-            addresses.push( addr.trim() );
-        }
-    });
-    return addresses;
+    return matchUnique(text, addressRegex).map(addr => addr.trim());
 }
 
 function identifyCities(text) {
     const places = nlp(text).places().out('array');
     const cities = [];
-    
+
     places.forEach(place => {
-        if (knownCities.includes(place) || !/\b(para|de|em)\b/i.test(place)) {
-            if (!cities.includes(place)) {
-                cities.push(place);
-            }
+        if (
+            knownCities.includes(place) ||
+            !/\b(para|de|em)\b/i.test(place)
+        ) {
+            uniquePush(cities, place);
         }
     });
 
-    // retorna cities sem caracteres especiais
     return cities.map(city => city.replace(/[^\w\s]/g, ''));
 }
 
 function identifyEmails(text) {
     const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
-    const emails = [];
-    (text.match(emailRegex) || []).forEach(email => {
-        if (validator.isEmail(email) && !emails.includes(email)) {
-            emails.push(email);
-        }
-    });
-    return emails;
+    return matchUnique(text, emailRegex, validator.isEmail);
 }
 
 function identifyNumbers(text) {
     const numberRegex = /\+?\d{1,3}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g;
-    const numbers = [];
-    (text.match(numberRegex) || []).forEach(number => {
-        if (!numbers.includes(number)) {
-            numbers.push(number);
-        }
-    });
-    return numbers;
+    return matchUnique(text, numberRegex);
 }
 
 function identifyPasswords(text) {
     const passwordRegex = /\b(?=[A-Za-z0-9@#$%^&+=]*[A-Z])(?=[A-Za-z0-9@#$%^&+=]*[a-z])(?=[A-Za-z0-9@#$%^&+=]*\d)[A-Za-z0-9@#$%^&+=]{8,}\b/g;
-    const passwords = [];
-    (text.match(passwordRegex) || []).forEach(password => {
-        if (!passwords.includes(password)) {
-            passwords.push(password);
-        }
-    });
-    return passwords;
+    return matchUnique(text, passwordRegex);
 }
 
 function identifyNationalIds(text) {
     const nationalIdRegex = /\b\d{3}\.\d{3}\.\d{3}-\d{2}\b|\b\d{2}\.\d{3}\.\d{3}\/?\d{4}-?\d{2}\b|\b\d{1,2}\.\d{3}\.\d{3}-?\d\b/g;
-    const nationalIds = [];
-    (text.match(nationalIdRegex) || []).forEach(id => {
-        if (!nationalIds.includes(id)) {
-            nationalIds.push(id);
-        }
-    });
-    return nationalIds;
+    return matchUnique(text, nationalIdRegex);
 }
 
 function identifyPassports(text) {
     const passportRegex = /\b[A-Z]{1,2}\d{6,8}\b/g;
-    const passports = [];
-    (text.match(passportRegex) || []).forEach(passport => {
-        if (!passports.includes(passport)) {
-            passports.push(passport);
-        }
-    });
-    return passports;
+    return matchUnique(text, passportRegex);
 }
 
 function identifyDrivingLicenses(text) {
     const drivingLicenseRegex = /\b[A-Z]{3}-?\d{4}\b|\b[A-Z]{3}\d[A-Z]\d{2}\b/g;
-    const drivingLicenses = [];
-    (text.match(drivingLicenseRegex) || []).forEach(license => {
-        if (!drivingLicenses.includes(license)) {
-            drivingLicenses.push(license);
-        }
-    });
-    return drivingLicenses;
+    return matchUnique(text, drivingLicenseRegex);
 }
 
 function identifyTaxIds(text) {
     const taxIdRegex = /\b\d{2}\.\d{3}\.\d{3}\/?\d{4}-?\d{2}\b/g;
-    const taxIds = [];
-    (text.match(taxIdRegex) || []).forEach(taxId => {
-        if (!taxIds.includes(taxId)) {
-            taxIds.push(taxId);
-        }
-    });
-    return taxIds;
+    return matchUnique(text, taxIdRegex);
 }
 
 function identifyHealthIds(text) {
     const healthIdRegex = /\b\d{10,12}\b/g;
-    const healthIds = [];
-    (text.match(healthIdRegex) || []).forEach(healthId => {
-        if (!healthIds.includes(healthId)) {
-            healthIds.push(healthId);
-        }
-    });
-    return healthIds;
+    return matchUnique(text, healthIdRegex);
 }
 
 function identifyOtherDocuments(text) {
     const otherDocumentRegex = /\b[A-Za-z]{2,3}\d{6,10}\b/g;
-    const otherDocuments = [];
-    (text.match(otherDocumentRegex) || []).forEach(doc => {
-        if (!otherDocuments.includes(doc)) {
-            otherDocuments.push(doc);
-        }
-    });
-    return otherDocuments;
+    return matchUnique(text, otherDocumentRegex);
 }
 
 function identifySensitiveDocuments(text) {
